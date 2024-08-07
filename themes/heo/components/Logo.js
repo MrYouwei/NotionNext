48ccc976127d211e2bc52e1de477c6f5441c5796
tanwb48177 <1550154864@qq.com>
2024-08-07 16:28:45 +0800

    update

--- themes/heo/components/Logo.js
+++ themes/heo/components/Logo.js
@@ -10,10 +10,11 @@ const Logo = props => {
       <div className='flex flex-nowrap items-center cursor-pointer font-extrabold'>
         <LazyImage
           src={siteInfo?.icon}
-          width={24}
-          height={24}
+          width={28}
+          height={28}
           alt={siteConfig('AUTHOR')}
           className='mr-4 hidden md:block'
+          style={{ borderRadius: '50%', overflow: 'hidden' }}
         />
         <div id='logo-text' className='group rounded-2xl flex-none relative'>
           <div className='group-hover:opacity-0 opacity-100 visible group-hover:invisible text-lg my-auto rounded dark:border-white duration-200'>
--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: '#' },
-    { title: '我的chatGPT', url: 'https://chat.youwei.icu' }
+    { title: '我的MyGPT', url: 'https://chat.youwei.icu' }
   ],
 
   // 英雄区左右侧组件颠倒位置
